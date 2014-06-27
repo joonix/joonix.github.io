917b3dece93e5c727813a6f0f34481e5d0386e06
Johnny Bergström <github@joonix.se>
2014-06-27 15:34:08 +0200

    Add quote form input validation visuals

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('joonix', ['ngRoute', 'controllers'])
-	.config(['$routeProvider', function($routeProvider) {
+	.config(function($routeProvider) {
 		$routeProvider
 			.when('/quote', {
 				templateUrl: 'partials/quote.html',
@@ -15,4 +15,17 @@ angular.module('joonix', ['ngRoute', 'controllers'])
 			.otherwise({
 				redirectTo: '/home'
 			})
-	}])
+	})
+	.config(function($compileProvider) {
+		// Add skype to the default list of href protocols
+		$compileProvider.aHrefSanitizationWhitelist(
+			/^\s*(https?|ftp|mailto|tel|file|skype):/
+		);
+	})
+	.factory('apiUrl', function($location) {
+		if ($location.host() == 'localhost') {
+			return 'http://localhost:3000';
+		} else {
+			return 'http://api.joonix.se';
+		}
+	});
--- js/controllers.js
+++ js/controllers.js
@@ -1,40 +1,7 @@
 'use strict';
 
 angular.module('controllers', [])
-	.constant('apiUrl', 'http://localhost:3000')
-	.config(function($compileProvider) {
-		// Add skype to the default list of href protocols
-		$compileProvider.aHrefSanitizationWhitelist(
-			/^\s*(https?|ftp|mailto|tel|file|skype):/
-		);
-	})
-	.controller('QuoteCtrl', function($scope, $log, $http, apiUrl) {
-		$scope.send = function() {
-			var message = "";
-			message += "Name: " + $scope.quote.name + "\n";
-			message += "Business: " + $scope.quote.business + "\n";
-			message += "Phone: " + $scope.quote.phone + "\n";
-			message += "------\n\n";
-			message += $scope.quote.message;
-
-			$http.post(apiUrl + "/message", {
-				from: $scope.quote.email,
-				message: message
-			})
-				.success(function() {
-					$log.info("It's a success!")
-				})
-				.error(function(msg) {
-					$log.info(msg);
-				});
-		}
-	})
-	.controller('NavCtrl', function($scope, $rootScope, $location) {
-		$scope.active = function(path) {
-			return $location.path() == path;
-		}
-	})
-	.controller('SocialCtrl', function($scope) {
+	.factory('contact', function contactFactory() {
 		// Variant of generated code from: http://www.jottings.com/obfuscator/
 		var decrypt = function(coded, key) {
 			var shift = coded.length
@@ -53,15 +20,54 @@ angular.module('controllers', [])
 		}
 
 		// Spam bots be gone
-		$scope.mailto = decrypt(
-			"W0p9@8990W4.5J",
-			"oABfHEwGK2YnPX9QgpUM71ybZ0aNsTvujhel3LzIcq5idV8xJDCrSkROtWF6m4");
+		return {
+			mailto: decrypt(
+				"W0p9@8990W4.5J",
+				"oABfHEwGK2YnPX9QgpUM71ybZ0aNsTvujhel3LzIcq5idV8xJDCrSkROtWF6m4"),
+			skype: decrypt(
+				"1p91Cpx?5p99",
+	  			"Oy8eBS6PEC0sIWqnAaFbRuzlHw7hmpg1Nct9v2VxZrJLk5Qd3KiMXYUGf4DoTj"),
+			phone: decrypt(
+				"+EUXooIdpEdd",
+	  			"U3VHjB1847gbxpD2wGiXERuQcaOdtWrJAqFny0mY9e5vZszLTS6NIKoMPfCkhl")
+		}
+	})
+	.controller('QuoteCtrl', function($scope, $log, $http, $location, contact, apiUrl) {
+		$scope.mailto = contact.mailto;
+		$scope.send = function() {
+			$scope.success = false;
+			$scope.error = false;
 
-		$scope.skype = decrypt(
-			"1p91Cpx?5p99",
-  			"Oy8eBS6PEC0sIWqnAaFbRuzlHw7hmpg1Nct9v2VxZrJLk5Qd3KiMXYUGf4DoTj");
+			var message = "";
+			message += "Name: " + $scope.quote.name + "\n";
+			message += "Business: " + $scope.quote.business + "\n";
+			message += "Phone: " + $scope.quote.phone + "\n";
+			message += "------\n\n";
+			message += $scope.quote.message;
 
-		$scope.phone = decrypt(
-			"+EUXooIdpEdd",
-  			"U3VHjB1847gbxpD2wGiXERuQcaOdtWrJAqFny0mY9e5vZszLTS6NIKoMPfCkhl");
+			$http.post(apiUrl + "/message", {
+				from: $scope.quote.email,
+				message: message
+			})
+				.success(function() {
+					$scope.success = true;
+					$scope.error = false;
+					$scope.quote = {};
+				})
+				.error(function(data, status) {
+					$scope.success = false;
+					data = data || "Request failed";
+					$scope.error = status + " - " + data;
+				});
+		}
+	})
+	.controller('NavCtrl', function($scope, $rootScope, $location) {
+		$scope.active = function(path) {
+			return $location.path() == path;
+		}
+	})
+	.controller('SocialCtrl', function($scope, contact) {
+		$scope.mailto = contact.mailto;
+		$scope.skype = contact.skype;
+		$scope.phone = contact.phone;
 	});
